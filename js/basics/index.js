0ed3a3c6e642e649343744626b470103696917b5
YusuufIbrahim <63614555+YusuufIbrahim@users.noreply.github.com>
2020-05-07 22:13:57 -0700

    Add files via upload

---
+++ js/basics/index.js
@@ -0,0 +1,22 @@
+// Console.log('Hello World');
+
+var firstName = 'John';
+
+var message = 'ITC-111'; // variable of type string
+
+var year = 2020; // variable of type number 
+
+var isTrue =true; //variable of type boolean
+
+
+var someVar;
+
+
+function printToConsole(msg){
+    // console.log(msg);
+    var what = msg + 'Javascript';
+    return what;
+}
+console.log(message));
+console.log(printToConsole(message));
+console.log(what));
